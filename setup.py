ff354eee6486c711d9b8d1a7e33b97a86270f8dc
ghedinip <pierfrancesco.ghedini@gmail.com>
2024-04-30 22:50:50 +0200

    Add existing files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="othello_ai",
+    version="0.0.1",
+    install_requires=["gymnasium==0.29.1",
+                      "pygame==2.1.0",
+                      "numpy==1.26.4"],
+)
