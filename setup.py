e5dcfe613c4520eec09644c136bab92b943bae2d
pghedini <pierfrancesco.ghedini@gmail.com>
2024-05-01 09:07:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="othello_ai",
     version="0.0.1",
     install_requires=["gymnasium==0.29.1",
-                      "pygame==2.1.0",
+                      "pygame>=2.1.0",
                       "numpy==1.26.4"],
 )
